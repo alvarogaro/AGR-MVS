2fc04441aa25694736f1f68234961a45a3d08d8f
pablo1mc315 <pablo1.lafuente@hotmail.com>
2022-10-25 16:53:35 +0200

    avanzo tarea #7 creando una clase para el objeto valor Empresa

---
+++ src/empresa.py
@@ -0,0 +1,18 @@
+# Clase Empresa (objeto valor que guarda los datos de cada empresa)
+
+from dataclasses import dataclass
+
+@dataclass
+class Empresa:
+    nombre: str             # Nombre de la empresa (string)
+    listaTiendas: list      # Lista de Tiendas que pertenecen a esta empresa (lista de datos de tipo Tienda)
+    
+    # Getters #
+
+    # Método get para obtener el nombre de una tienda
+    def getNombre (self):
+        return self.nombre
+
+    # Método get para obtener las tiendas que pertenecen a la empresa
+    def getListaTiendas (self):
+        return self.listaTiendas
