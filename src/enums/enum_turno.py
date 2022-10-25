a40eb7cc061a182eb0171998d8adfedb4464c830
pablo1mc315 <pablo1.lafuente@hotmail.com>
2022-10-25 16:37:37 +0200

    avanzo tarea #7 creando el enumerado para los turnos (necesita modificación más adelante)

---
+++ src/enums/enum_turno.py
@@ -0,0 +1,8 @@
+# Enum Turno (enumerado que indica los tres tipos de turnos de trabajo que hay en una empresa)
+
+from enum import Enum
+
+class Turno (Enum):
+    MANIANA = 1
+    TARDE = 2
+    NOCHE = 3
