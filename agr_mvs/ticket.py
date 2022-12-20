3dfef318b682a32a6818e0f9c5bdf966040228a4
Alvaro García Rodríguez <alvarogaro@correo.ugr.es>
2022-12-20 13:46:46 +0100

    Reestructuracion atributos #20

--- agr_mvs/ticket.py
+++ agr_mvs/ticket.py
@@ -9,4 +9,8 @@ class Ticket:
     
     def __init__(self, id, fecha_ticket):
         self.id = id
+<<<<<<< HEAD
         self.FechaTicket = fecha_ticket
+=======
+        self.FechaTicket = fecha_ticket
+>>>>>>> e6b8f15 (Reestructuracion atributos #20)
